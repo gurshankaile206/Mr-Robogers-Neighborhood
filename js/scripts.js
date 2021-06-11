a649e6a74ab79ffe808ce015e69e562769e70528
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-11 10:52:25 -0700

    add second & 3rd test function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 // Business logic
 
 function beepBoop(input) {
-    if (input === 0) {
+    if  (input === 0) {
         return "0";
     }
+    if  (input === 1) {
+        return "Beep"
+    }
+    if  (input === 2) {
+        return ""
+    }
+    if  (input)
 };
 

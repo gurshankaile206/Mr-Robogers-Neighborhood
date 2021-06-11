db122e40e9fb0b96ebe79f13adf93a6359172b48
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-11 14:10:38 -0700

    add functionality that allows the functiont to output inoformation from most important to least important

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,13 @@
 // Business logic
 
 function beepBoop(input) {
-    if  (input === 0) {
-        return "0";
-    }
-    if  (input === 1) {
-        return "Beep"
-    }
-    if  (input === 2) {
-        return "Boop"
-    }
-    if (input === 3) {
-        return "Won't you be my neighbor?";
-    }
     if (!Number.isInteger(input)) {
         return "input is not an number";
     }
-    let string = input.toString();
+    let string = input.toString().split('');
+    if  (string.includes('0')) {
+        return "0";
+    } if (string.includes('3')) {
+        return "Won't you be my neighbor?";
+    }
 };
-

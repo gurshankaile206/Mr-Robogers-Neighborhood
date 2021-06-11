6b53f9e112a4317b5cc9cb9468d627e40e92a29c
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-11 16:35:05 -0700

    got stuck current code in JS is where I am stuck at

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,23 @@ function range(input) {
     }
     return output
 }
+
+function beepBoop(input) {
+    let count = 0; 
+    let output = [];
+    do{
+        let string = input.toString().split('');
+        if  (string.includes('0')) {
+            return "0";
+        } if (string.includes('3')) {
+            return "Won't you be my neighbor?";
+        } else if  (string.includes('2')) {
+            return "Boop"
+        } else if  (string.includes('1')) {
+            return "Beep"
+        } else {
+            output.push(count)
+            count++
+        }
+    }while(count <= parseInt(input));
+}
